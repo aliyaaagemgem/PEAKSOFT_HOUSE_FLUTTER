d010850428a18f08ebeef1f71b8a0bbdc1a6f79c
Aliya <alyiaabdykadyrova@gmail.com>
2024-03-15 18:13:52 +0600

    done

---
+++ operators_var_dynamic_const_finall/bin/operators_var_dynamic_const_finall.dart
@@ -0,0 +1,5 @@
+import 'package:operators_var_dynamic_const_finall/operators_var_dynamic_const_finall.dart' as operators_var_dynamic_const_finall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${operators_var_dynamic_const_finall.calculate()}!');
+}
---
+++ operators_var_dynamic_const_finall/test/operators_var_dynamic_const_finall_test.dart
@@ -0,0 +1,8 @@
+import 'package:operators_var_dynamic_const_finall/operators_var_dynamic_const_finall.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
