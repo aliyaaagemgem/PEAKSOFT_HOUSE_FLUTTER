0276b711ba61c12c03e3db23787efb41002d8f1d
Aliya <alyiaabdykadyrova@gmail.com>
2024-03-20 18:54:36 +0600

    collections

---
+++ collections_1/bin/collections_1.dart
@@ -0,0 +1,33 @@
+import 'package:collections_1/collections_1.dart' as collections_1;
+
+void main() {
+  // int a = 1;
+  // int b = 2;
+  // int c =3;
+  // print('$a,$b,$c');
+
+  // Collections
+
+  // List;
+//   List numbers = [1,2,3,100,200,5,8,10];
+//   print(numbers[1]);
+// List names = ['Asan', 'Uson', 'Samat', 'Bayish'];
+// names.add('Baizak');
+// print(names);
+
+
+// Set setName = {
+//   'Asan',
+//   'Uson',
+//   'Bayish',
+//   'Baizak',
+// };
+// print(setName);
+
+// Map
+Map mapName = {
+  'name': 'Asan', 
+  'animal': 'Cow',
+  'color': 'White'};
+  print(mapName['color']);
+ }
---
+++ collections_1/test/collections_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:collections_1/collections_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
