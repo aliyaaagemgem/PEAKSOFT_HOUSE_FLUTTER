09d0e879b27ee174c56e19f9d6946dfd93de6dca
Aliya <alyiaabdykadyrova@gmail.com>
2024-04-27 19:52:03 +0600

    done

---
+++ second_page/lib/second_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  final int secontCounter;
+
+  const SecondPage({super.key, required this.secontCounter});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SecondPage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$secontCounter',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
